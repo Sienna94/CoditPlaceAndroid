e4483e4f8451ee5eebcc77aa84fb7be49c3e5cd3
Sienna94 <csj19940407@gmail.com>
2021-01-17 23:23:40 +0900

    북마크 리스트뷰 삭제

--- app/src/main/java/com/example/coditplace2/ItemDataBk.java
+++ app/src/main/java/com/example/coditplace2/ItemDataBk.java
@@ -1,11 +1,13 @@
 package com.example.coditplace2;
 
 public class ItemDataBk {
+    int bKidx;
     String pIdx;
     String pName;
     String pIcon;
 
-    public ItemDataBk(String pIdx, String pName, String pIcon) {
+    public ItemDataBk(int bKidx, String pIdx, String pName, String pIcon) {
+        this.bKidx = bKidx;
         this.pIdx = pIdx;
         this.pName = pName;
         this.pIcon = pIcon;
--- app/src/main/java/com/example/coditplace2/MypageFrag2.java
+++ app/src/main/java/com/example/coditplace2/MypageFrag2.java
@@ -19,7 +19,7 @@ import com.bumptech.glide.Glide;
 
 import java.util.ArrayList;
 
-public class MypageFrag2 extends BaseFrag{ //북마크 페이지
+public class MypageFrag2 extends BaseFrag implements View.OnClickListener{ //북마크 페이지
     ArrayList<ItemDataBk> arr = new ArrayList<>();
     TextView tv;
     ListView lv;
@@ -41,20 +41,33 @@ public class MypageFrag2 extends BaseFrag{ //북마크 페이지
     }
     //리스트뷰 테스트
     public void test(){
-        arr.add(0, new ItemDataBk("1","아일랜드팩토리 풍류",  "picon"));
-        arr.add(1, new ItemDataBk("2","프릳츠 양재점",  "picon"));
-        arr.add(2, new ItemDataBk("3","와겐커피 미아점(본점)",  "picon"));
+        arr.add(0, new ItemDataBk(0,"1","아일랜드팩토리 풍류",  "picon"));
+        arr.add(1, new ItemDataBk(1,"2","프릳츠 양재점",  "picon"));
+        arr.add(2, new ItemDataBk(2,"3","와겐커피 미아점(본점)",  "picon"));
         Log.d("chk", "test: 실행되냐"+arr.get(0).pName+arr.get(1).pName+arr.get(2).pName);
         //어댑터 적용
         adapter = new MyAdapter(getActivity());
         lv.setAdapter(adapter);
     }
+    //테스트 삭제
+    public void test_del(int bKidx){
+        arr.remove(position);
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+
     class ItemHolder {
         TextView tvPnameHolder;
         ImageView ivPiconHolder;
         TextView tvGetpostHolder;
         TextView tvDelbkHolder;
     }
+    int position; // 액티비티 안의 position을 말한다! myadapter안의 position이 아니라 액티비티 포지션을 찾아서 지워줘야되니까.
     class MyAdapter extends ArrayAdapter {
         LayoutInflater lnf;
 
@@ -96,6 +109,17 @@ public class MypageFrag2 extends BaseFrag{ //북마크 페이지
             }
             //가게 아이콘, 가게 이름,
             viewHolder.tvPnameHolder.setText(arr.get(position).pName);
+            //북마크 삭제 클릭
+            viewHolder.tvDelbkHolder.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v) {
+                    Log.d("chk", "북마크 삭제 클릭");
+                    MypageFrag2.this.position = position;
+                    int bKidx = arr.get(position).bKidx;
+                    Log.d("chk", "북마크 삭제, bKidx :" + bKidx);
+                    test_del(bKidx);
+                }
+            });
 
 ////            카페 사진
 //            Glide.with(getActivity())
